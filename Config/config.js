240d11409acb6cd26381dfbc7a9d121ad2314d54
MonteiroJ <josemonteiro.baggio@gmail.com>
2023-10-13 18:48:05 +0200

    New initial commit

--- Config/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    token: 'MTE2MjM4OTgxNjI3NzczMzQ4Nw.GRN2S2.eifLbLkkAuLnaP93iBrMameqEMLrbXIg6eYNZM'
-};
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const Discord = require('discord.js');
 const intents = new Discord.IntentsBitField(3276799);
 const bot = new Discord.Client({intents});
 const loadCommands = require('./Loader/loadCommands');
 const loadEvents = require('./Loader/loadEvents');
-const config = require('./Config/config');
+
 
 bot.commands = new Discord.Collection();
 
-bot.login(config.token);
+bot.login(process.env.token);
 loadCommands(bot);
-//loadEvents(bot);
+loadEvents(bot);
