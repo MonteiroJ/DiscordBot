e393be80e924109dced9a112e1ab441d7ed21906
MonteiroJ <josemonteiro.baggio@gmail.com>
2023-10-13 18:55:41 +0200

    Add autocompletion for slash commands

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -2,6 +2,13 @@ const Discord = require('discord.js');
 
 module.exports = async (bot, interaction) => {
 
+    if (interaction.type === Discord.InteractionType.ApplicationCommandAutocomplete) {
+        
+        let entry = interaction.options.getFocused();
+        let choices = bot.commands.filters(cmd => cmd.name.includes(entry));
+        await interatction.respond(entry === '' ? bot.commands.map(cmd => ({name: cmd.name, value: cmd.name})) : choices.map(choice => ({name: choice.name, value: choice})));
+    }
+
     if (interaction.type === Discord.InteractionType.ApplicationCommand) {
 
         let command = require(`../Commands/${interaction.commandName}.js`);
